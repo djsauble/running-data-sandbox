c98fbe3ee1b123d1d58535a4ab99e549ba48533c
Daniel Sauble <djsauble@gmail.com>
2016-05-23 14:51:08 -0700

    Display map and center it on the first coordinate in the database

--- ui.js
+++ ui.js
@@ -1,10 +1,43 @@
+var map;
+
 /* Initialize the data-sensitive pieces of the UI */
 function initUI() {
   // Configure event handlers
-  document.getElementById("input").addEventListener('input', function() {
-    console.log("Input changed");
+  document.getElementById("input").addEventListener('input', function(cmp) {
+    eval(cmp.target.value);
   });
 
   // Wire up the refresh button
   document.getElementById("nav").innerHTML = "<a href='javascript:refreshData();'>Refresh cache</a>";
+
+  // Initialize the map
+  initMap();
+}
+
+/* Show the map */
+function initMap() {
+
+  return new Promise(function(resolve, reject) {
+    db.allDocs({include_docs: true, attachments: true, limit: 1}).then(function(results) {
+
+      /* Calculate the the center of the map */
+      var data = JSON.parse(
+                   atob(
+                     results.rows[0].doc._attachments["data.json"]["data"]
+                   )
+                 );
+      var lat = parseFloat(data[0]["latitude"]);
+      var lng = parseFloat(data[0]["longitude"]);
+
+      /* Display the map */
+      map = new google.maps.Map(document.getElementById('output'), {
+        center: {lat: lat, lng: lng},
+        zoom: 12,
+        disableDefaultUI: true,
+        draggable: false
+      });
+
+      resolve();
+    });
+  });
 }
