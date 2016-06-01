da0f9e7ac0a5697e073268a3f53c5b39217af957
Daniel Sauble <djsauble@gmail.com>
2016-05-31 21:16:28 -0700

    Add accuracy filtering to app

--- app/ui.js
+++ app/ui.js
@@ -157,32 +157,41 @@ class UI {
 
   // Smooth the run (e.g. ignore bouncing GPS tracks)
   defaultFilter(data) {
-    var filtered = [];
-    var maxDistance = 30; // Meters
+    var accurate = [],
+        filtered = [],
+        maxDistance = 30; // Meters
 
-    for (var i = 1; i < data.length - 1; ++i) {
+    // Filter out inaccurate points
+    data.forEach(function(e) {
+      if (parseFloat(e.accuracy) < maxDistance) {
+        accurate.push(e);
+      }
+    });
+
+    // Filter out discontinuities (points that aren't adjacent to any other points)
+    for (var i = 1; i < accurate.length - 1; ++i) {
       var pt1 = new google.maps.LatLng(
           {
-            lat: parseFloat(data[i-1].latitude),
-            lng: parseFloat(data[i-1].longitude)
+            lat: parseFloat(accurate[i-1].latitude),
+            lng: parseFloat(accurate[i-1].longitude)
           }
       );
       var pt2 = new google.maps.LatLng(
           {
-            lat: parseFloat(data[i].latitude),
-            lng: parseFloat(data[i].longitude)
+            lat: parseFloat(accurate[i].latitude),
+            lng: parseFloat(accurate[i].longitude)
           }
       );
       var pt3 = new google.maps.LatLng(
           {
-            lat: parseFloat(data[i+1].latitude),
-            lng: parseFloat(data[i+1].longitude)
+            lat: parseFloat(accurate[i+1].latitude),
+            lng: parseFloat(accurate[i+1].longitude)
           }
       );
       var d1 = google.maps.geometry.spherical.computeDistanceBetween(pt1, pt2);
       var d2 = google.maps.geometry.spherical.computeDistanceBetween(pt2, pt3);
       if (d1 <= maxDistance && d2 <= maxDistance) {
-        filtered.push(data[i]);
+        filtered.push(accurate[i]);
       }
     }
 
